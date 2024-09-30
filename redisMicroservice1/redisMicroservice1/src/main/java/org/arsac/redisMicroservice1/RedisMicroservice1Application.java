201ef7a66d77eefef602cbf14e2e4119ecd75ff7
ArsacMohammed <arsacskasha@gmail.com>
2024-09-30 20:17:49 +0530

    initial commit fresh branch

---
+++ redisMicroservice1/redisMicroservice1/src/main/java/org/arsac/redisMicroservice1/RedisMicroservice1Application.java
@@ -0,0 +1,13 @@
+package org.arsac.redisMicroservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisMicroservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisMicroservice1Application.class, args);
+	}
+
+}
---
+++ redisMicroservice1/redisMicroservice1/src/test/java/org/arsac/redisMicroservice1/RedisMicroservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.arsac.redisMicroservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisMicroservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
